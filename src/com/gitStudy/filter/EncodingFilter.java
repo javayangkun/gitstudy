e3cd0bb63079882a53a0a8a0705275ea5a6a9659
yang <yang@163.com>
2019-06-03 20:30:20 +0800

    增加了控制层

--- src/com/gitStudy/filter/EncodingFilter.java
+++ src/com/gitStudy/filter/EncodingFilter.java
@@ -9,29 +9,17 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 
-/**
- * Servlet Filter implementation class EncodingFilter
- */
 @WebFilter("/EncodingFilter")
 public class EncodingFilter implements Filter {
 
-    /**
-     * Default constructor. 
-     */
     public EncodingFilter() {
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see Filter#destroy()
-	 */
 	public void destroy() {
 		// TODO Auto-generated method stub
 	}
 
-	/**
-	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
-	 */
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 		// TODO Auto-generated method stub
 		// place your code here
@@ -40,9 +28,6 @@ public class EncodingFilter implements Filter {
 		chain.doFilter(request, response);
 	}
 
-	/**
-	 * @see Filter#init(FilterConfig)
-	 */
 	public void init(FilterConfig fConfig) throws ServletException {
 		// TODO Auto-generated method stub
 	}
